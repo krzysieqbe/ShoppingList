2fb5fadfc1e25080dbf9160bed0838ff5d0f4b6c
Krzyieqbe <krzysieqbe@github.com>
2020-03-16 10:48:13 +0100

    items list

--- js/index.js
+++ js/index.js
@@ -1,10 +1,28 @@
 var handleKeyPress = function(e) {
     var key = e.keyCode || e.which;
     if (key == 13) {
-        console.log('dupa');
+        addItem();
     }
 };
 
-handleKeyPress({
-    keyCode: 13
-});
+
+function addItem() {
+    e = document.getElementById("item-name");
+    var item = e.value;
+    e.value = "";
+    var html = '<div class="products-list-item">';
+    html += '<div>v</div>';
+    html += '<div>' + item + '</div>';
+    html += '<div>...</div>';
+    html += '<div><i class="fa fa-trash"></i></div>';
+
+    list = document.getElementById("products-list");
+    list.innerHTML += html;
+};
+
+document.onload = function() {
+    setTimeout(function() {
+        e = document.getElementById("item-name");
+        e.value = "";
+    }, 250);
+}();
